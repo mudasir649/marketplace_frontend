5194716e0ffe65cd78d4b50a55f9d3dcd7040136
mudasir <mudasirriaz649@gmail.com>
2023-08-29 22:03:31 +0500

    some bugs fixed

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -1,11 +1,18 @@
-import React, { useEffect } from 'react'
+import React, { useCallback, useEffect, useRef, useState } from 'react'
 import { Agriculture, CarRepair, DirectionsBike, DirectionsCar, KeyboardArrowDown, LocalShipping, TwoWheeler } from '@mui/icons-material';
 import Aos from 'aos';
 import SearchPage from './Search';
 import useWindowDimensions from '@/utils/useWindowDimensions';
+import CategoryList from './CategoryList';
 
 
 export default function Banner() {
+
+  const [isExpand, setIsExpand] = useState<Boolean>(false);
+  const [allCategory, setAllCategory] = useState<string>("");
+  const [category, setCategory] = useState<string>("");
+  const dropdownRef = useRef<HTMLDivElement | null>(null);
+
 
   useEffect(() => {
     Aos.init();
@@ -16,29 +23,57 @@ export default function Banner() {
   const newWidth = width || 0;
   const newHeight = height || 0;
 
+  console.log(category);
+  console.log(allCategory);
+
+  const closeDropdown = () => {
+    setIsExpand(false);
+  }
+
+  const handleOutsideClick = useCallback(
+    (event: MouseEvent) => {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target as Node)) {
+        closeDropdown();
+      }
+    },
+    []
+  );
+
+  useEffect(() => {
+    document.addEventListener('click', handleOutsideClick);
+
+    return () => {
+      document.removeEventListener('click', handleOutsideClick);
+    }
+  }, [handleOutsideClick])
+
+
+
   return (
     <div className='mb-5  border-green-400 mt-1 h-[300px]'>
       <div className='container mx-auto'>
         <div className='flex flex-col md:flex-row lg:space-x-10'>
-          <div className='flex flex-row justify-between border border-gray-200 rounded-full h-8 w-52 hover:border-red-500 
-              bg-white px-5 py-1 capitalize text-sm'>
-            <h1>See all categories</h1>
-            <KeyboardArrowDown className="h-5 w-5 border border-red-500 rounded-full bg-red-500 text-white" />
+          <div className='flex flex-col' ref={dropdownRef}>
+            <div className={`flex flex-row justify-between border border-gray-200 rounded-full h-8 w-52 hover:border-red-500 
+              bg-white px-5 py-1 capitalize text-sm focus:border-red-800 focus:shadown-lg`} onClick={() => setIsExpand(!isExpand)}>
+              <h1>See all categories</h1>
+              <KeyboardArrowDown className="h-5 w-5 border border-red-500 rounded-full bg-red-500 text-white" />
+            </div>
+            {isExpand && <div className='h-auto p-2 w-auto lg:w-52 z-10 absolute bg-white border rounded-md mt-9' data-aos="fade-up">
+              <CategoryList setCategory={setAllCategory} setExpand={setIsExpand} />
+            </div>}
           </div>
           <div className='felx flex-row mt-4 lg:mt-0'>
             <h1 className='text-white text-lg font-semibold flex space-x-5'>
               Top Categories:
-              <span className='ml-4'>Autos</span>
-              <span>Parts</span>
+              <span className='ml-4 cursor-pointer' onClick={() => setCategory("Autos")}>Autos</span>
+              <span className='cursor-pointer' onClick={() => setCategory("Parts")}>Parts</span>
             </h1>
           </div>
         </div>
       </div>
       <div className='pt-10'>
         <SearchPage />
-        {/* <h1 className='text-4xl lg:text-[58px] font-semibold leading-none'>
-            <span className='text-[#e52320]'>Buy</span> <span className='text-black'>What ever you want.</span>
-          </h1> */}
       </div>
     </div >
   )
--- components/KeywordInputField.tsx
+++ components/KeywordInputField.tsx
@@ -1,5 +1,6 @@
+'use client';
 import { Category, ExpandLess, ExpandMore, FmdGood } from '@mui/icons-material';
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
 import Autocomplete, { usePlacesWidget } from "react-google-autocomplete";
 import CategoryList from './CategoryList';
 import Aos from 'aos';
@@ -12,6 +13,7 @@ export default function KeywordInputField({ logo, text1, text2 }: any) {
   const [keyword, setKeyword] = useState<string>("");
   const componentText = text2;
   const [expand, setExpand] = useState<Boolean>(false);
+  const dropdownRef = useRef<HTMLDivElement | null>(null)
 
   const { ref } = usePlacesWidget({
     apiKey: process.env.NEXT_PUBLIC_GOOGLE_MAP_API,
@@ -45,16 +47,32 @@ export default function KeywordInputField({ logo, text1, text2 }: any) {
     }
   }
 
-  console.log(address);
+  const closeDropdown = () => {
+    setExpand(false);
+  }
 
+  const handleMouseClick = useCallback((
+    (event: MouseEvent) => {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target as Node)) {
+        closeDropdown();
+      }
+    }
+  ), []);
+
+  useEffect(() => {
+    document.addEventListener('click', handleMouseClick);
+    return () => {
+      document.removeEventListener('click', handleMouseClick);
+    }
+  }, [handleMouseClick])
 
   return (
     <div>
       <div className='border ring-gray-200 rounded-lg hover:border-red-500 focus:outline-red-600  
-      cursor-pointer transition w-56 h-16 lg:w-72 lg:h-16' onClick={(e) => handleClick(e)}>
+      cursor-pointer transition w-auto h-16 lg:w-72 lg:h-16' onClick={(e) => handleClick(e)} ref={dropdownRef}>
         <div className='container mx-auto flex flex-row w-full space-x-3 lg:space-x-6 p-2'>
           <div><Logo /></div>
-          <div className='flex flex-col w-32 space-y-[0.5px]'>
+          <div className='flex flex-col w-auto space-y-[0.5px]'>
             <div>{text1}</div>
             {logo === "component2" ?
               <div>{category === "" ? componentText : category}</div>
@@ -66,13 +84,13 @@ export default function KeywordInputField({ logo, text1, text2 }: any) {
                       let { formatted_address } = place;
                       setAddress(formatted_address || "");
                     }}
-                    className='border-none w-40 lg:w-52 bg-transparent focus:outline-none'
+                    className='border-none w-auto lg:w-52 bg-transparent focus:outline-none'
                     placeholder={text2}
                   />
                 </div>
                 :
                 <div><input type='text' value={keyword}
-                  className='border-none w-40 lg:w-52 mt-2 bg-transparent focus:outline-none'
+                  className='border-none w-auto lg:w-52 mt-2 bg-transparent focus:outline-none'
                   placeholder={text2} onChange={(e: any) => setKeyword(e.target.value)} /></div>
             }
           </div>
@@ -81,7 +99,7 @@ export default function KeywordInputField({ logo, text1, text2 }: any) {
       </div>
       {logo === "component2" &&
         <div>
-          {expand ? <div className='h-auto p-2 w-56 lg:w-72 z-10 absolute bg-white border rounded-md mt-1' data-aos="fade-up">
+          {expand ? <div className='h-auto p-2 w-auto lg:w-72 z-10 absolute bg-white border rounded-md mt-1' data-aos="fade-up">
             <CategoryList setCategory={setCategory} setExpand={setExpand} />
           </div> : ''}
         </div>
