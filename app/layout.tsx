e6ff102a7f17975c9a362c2194f860cbbc3e4b23
mudasir <mudasirriaz649@gmail.com>
2023-09-15 15:06:34 +0500

    env files ignore

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,16 @@
 'use client'
 import './globals.css'
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { Provider } from "react-redux";
 import store from '@/store/store';
+import { Inter } from 'next/font/google'
 
 
 const inter = Inter({ subsets: ['latin'] })
 
-const metadata: Metadata = {
+export const metadata: Metadata = {
   title: 'Eidcarosse',
   description: 'Welcome to Eidcarosse - No.1 Autos Buy and Sell Marketplace. Explore a diverse range of autos, connect with Swiss buyers and sellers',
 }
