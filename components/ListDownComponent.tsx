8c21bcf2109d5e47f7c04cbd5559299fe80606c1
Usman Shafique <usmanshafique4342@gmail.com>
2023-10-18 15:05:03 +0500

    Functionality Addded to language change

--- components/ListDownComponent.tsx
+++ components/ListDownComponent.tsx
@@ -1,8 +1,9 @@
 import { ArrowDropDown } from '@mui/icons-material'
 import React, { useState } from 'react'
+import i18n from 'i18next';
 
 export default function ListDownComponent() {
-    const [language, setLanguage] = useState<string>('En');
+    const [language, setLanguage] = useState<string>('en'); // Initialize with the default language 'en'
     const [showList, setShowList] = useState<Boolean>(false);
 
     const liStyle = 'cursor-pointer hover:text-[#FF0000] hover:w-7';
@@ -10,6 +11,7 @@ export default function ListDownComponent() {
     const handleLanguage = (value: string) => {
         setLanguage(value);
         setShowList(false);
+        i18n.changeLanguage(value); // Change the language using i18n
     }
 
     return (
@@ -20,11 +22,11 @@ export default function ListDownComponent() {
             </div>
             {showList &&
                 <ul className="absolute bg-white text-black w-11 p-3 border space-y-2" data-aos="fade-up">
-                    <li className={liStyle} onClick={() => handleLanguage('EN')}>EN</li>
-                    <li className={liStyle} onClick={() => handleLanguage('DE')}>DE</li>
-                    <li className={liStyle} onClick={() => handleLanguage('FR')}>FR</li>
-                    <li className={liStyle} onClick={() => handleLanguage('ES')}>ES</li>
-                    <li className={liStyle} onClick={() => handleLanguage('IT')}>IT</li>
+                    <li className={liStyle} onClick={() => handleLanguage('en')}>en</li>
+                    <li className={liStyle} onClick={() => handleLanguage('de')}>de</li>
+                    <li className={liStyle} onClick={() => handleLanguage('fr')}>fr</li>
+                    <li className={liStyle} onClick={() => handleLanguage('es')}>es</li>
+                    <li className={liStyle} onClick={() => handleLanguage('it')}>it</li>
                 </ul>}
         </div>
     )
