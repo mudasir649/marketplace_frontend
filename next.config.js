94cf5286fff2658621993fb7d70a68f2d74ad88d
Usman Shafique <usmanshafique4342@gmail.com>
2023-11-20 16:52:45 +0500

    Next Config added

--- next.config.js
+++ next.config.js
@@ -1,27 +1,58 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    images: {
-        remotePatterns: [
-          {
-            protocol: 'http',
-            hostname: 'res.cloudinary.com',
-            port: '',
-            pathname: '/**',
-          },
-          {
-            protocol: 'https',
-            hostname: 'res.cloudinary.com',
-            port: '',
-            pathname: '/**',
-          },
-          {
-            protocol: 'https',
-            hostname: 'eidcarosse.ch',
-            port: '',
-            pathname: '/**',
-          },
-        ],
-      },
-}
+const { i18n } = require('i18next');
 
-module.exports = nextConfig
+module.exports = {
+  ...i18n, // Include i18n config
+
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'res.cloudinary.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'eidcarosse.ch',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+
+  async rewrites() {
+    const pages = [
+      'about-us',
+      'advance-search',
+      'change-password',
+      'chat',
+      'edit-ad',
+      'faq',
+      'forgot-password',
+      'how-to-sell-fast',
+      'login',
+      'my-ads',
+      'my-favourites',
+      'my-profile',
+      'post-ad',
+      'privacy-policy',
+      'product-details',
+      'search-filter',
+      'signup',
+      'terms-and-condition',
+      'verify-account',
+      // Add other pages as needed
+    ];
+
+    return pages.map((page) => ({
+      source: `/:lang/${page}`,
+      destination: `/${page}`,
+    }));
+  },
+};
