babd53ebc589c5ffafc3ff4e2594e1d63e309a22
Usman Shafique <usmanshafique4342@gmail.com>
2023-11-22 15:39:58 +0500

    new done Language index

--- components/ListDownComponent.tsx
+++ components/ListDownComponent.tsx
@@ -1,68 +1,52 @@
-import React, { useState, useEffect, useRef, useCallback } from 'react';
-import { KeyboardArrowDown } from '@mui/icons-material';
+import { ArrowDropDown, KeyboardArrowDown } from '@mui/icons-material'
+import React, { useState, useEffect, useRef, useCallback } from 'react'
 import i18n from 'i18next';
-import { Link } from 'react-scroll';
 
-export default function ListDownComponent({ setNavbar }: any) {
-  const [language, setLanguage] = useState<string>(i18n.language);
-  const [showList, setShowList] = useState<Boolean>(false);
-  const dropdownRef = useRef<HTMLDivElement | null>(null);
+export default function ListDownComponent({setNavbar}: any) {
+    const [language, setLanguage] = useState<string>(i18n.language); // Initialize with the current language
+    const [showList, setShowList] = useState<Boolean>(false);
+    const dropdownRef = useRef<HTMLDivElement | null>(null);
 
-  const handleOutsideClick = useCallback((event: MouseEvent) => {
-    if (dropdownRef.current && !dropdownRef.current.contains(event.target as Node)) {
-      setShowList(false);
+    const handleOutsideClick = useCallback((event: MouseEvent) => {
+        if(dropdownRef.current && !dropdownRef.current.contains(event.target as Node)){
+            setShowList(false);
+        }
+    }, [])
+
+    useEffect(() => {
+        document.addEventListener('click', handleOutsideClick);
+        return () => {
+            document.removeEventListener('click', handleOutsideClick);
+        }
+    }, [handleOutsideClick])
+
+    const liStyle = 'cursor-pointer hover:text-white hover:bg-[#FF0000] hover:border hover:border-[#FF0000] hover:rounded-sm hover:w-7 hover:p-1 ease-in duration-200';
+
+    const handleLanguage = (value: string, value2: string) => {
+        setLanguage(value2);
+        setShowList(false);
+        setNavbar(false)
+        i18n.changeLanguage(value);
     }
-  }, []);
 
-  useEffect(() => {
-    document.addEventListener('click', handleOutsideClick);
-    return () => {
-      document.removeEventListener('click', handleOutsideClick);
-    };
-  }, [handleOutsideClick]);
+    useEffect(() => {
+        setLanguage(i18n.language);
+    }, []);
 
-  const liStyle =
-    'cursor-pointer hover:text-white hover:bg-[#FF0000] hover:border hover:border-[#FF0000] hover:rounded-sm hover:w-7 hover:p-1 ease-in duration-200';
-
-  const handleLanguage = (value: string, value2: string) => {
-    setLanguage(value2);
-    setShowList(false);
-    setNavbar(false);
-    i18n.changeLanguage(value);
-  };
-
-  useEffect(() => {
-    setLanguage(i18n.language);
-  }, []);
-
-  return (
-    <div onClick={() => setShowList(!showList)} ref={dropdownRef}>
-      <div className="flex flex-row cursor-pointer">
-        <span>{language}</span>
-        <KeyboardArrowDown
-          fontSize="small"
-          className={`logo ${showList ? 'active' : 'inactive'} h-5 w-5 text-[#FF0000] mt-[0.8px]`}
-        />
-      </div>
-      {showList && (
-        <ul className="absolute bg-white text-black w-11 p-2 border space-y-2 z-20" data-aos="fade-up">
-          <li className={liStyle} onClick={() => handleLanguage('EN', 'EN')}>
-            <Link to="/en">EN</Link>
-          </li>
-          <li className={liStyle} onClick={() => handleLanguage('DE', 'DE')}>
-            <Link to="/de">DE</Link>
-          </li>
-          <li className={liStyle} onClick={() => handleLanguage('FR', 'FR')}>
-            <Link to="/fr">FR</Link>
-          </li>
-          <li className={liStyle} onClick={() => handleLanguage('ES', 'ES')}>
-            <Link to="/es">ES</Link>
-          </li>
-          <li className={liStyle} onClick={() => handleLanguage('IT', 'IT')}>
-            <Link to="/it">IT</Link>
-          </li>
-        </ul>
-      )}
-    </div>
-  );
+    return (
+        <div onClick={() => setShowList(!showList)} ref={dropdownRef}>
+            <div className="flex flex-row cursor-pointer">
+                <span>{language}</span>
+                <KeyboardArrowDown fontSize='small' className={`logo ${showList ? 'active': 'inactive'} h-5 w-5 text-[#FF0000] mt-[0.8px]`} />
+            </div>
+            {showList &&
+                <ul className="absolute bg-white text-black w-11 p-2 border space-y-2 z-20" data-aos="fade-up">
+                    <li className={liStyle} onClick={() => handleLanguage('EN', 'EN')}>EN</li>
+                    <li className={liStyle} onClick={() => handleLanguage('DE', 'DE')}>DE</li>
+                    <li className={liStyle} onClick={() => handleLanguage('FR', 'FR')}>FR</li>
+                    <li className={liStyle} onClick={() => handleLanguage('ES', 'ES')}>ES</li>
+                    <li className={liStyle} onClick={() => handleLanguage('IT', 'IT')}>IT</li>
+                </ul>}
+        </div>
+    )
 }
