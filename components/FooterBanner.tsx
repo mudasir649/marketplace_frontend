2ca2a4b0fceeab63d1b852f3d008216198cdcdd8
Usman Shafique <usmanshafique4342@gmail.com>
2023-11-29 16:27:05 +0500

    Subcateogry translation

--- components/FooterBanner.tsx
+++ components/FooterBanner.tsx
@@ -11,8 +11,7 @@ export default function FooterBanner() {
     return (
         <div className='mb-10 mt-1 h-auto p-5' data-aos="fade-down">
             <div className={`container mx-auto ${style.bg}`}>
-                {/*sName='text-[#FF0000]'>Buy</span> <span className='text-black'>What ever you want.</span>
-        </h1> */}
+                
             </div>
         </div >
     )
